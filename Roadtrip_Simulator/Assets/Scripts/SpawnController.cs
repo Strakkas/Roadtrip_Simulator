466ae3740a768d019e378a581662a58007d9ac68
Strakkas <benek.tysz@gmail.com>
2022-09-09 13:39:51 +0200

    Code cleanup Abstraction done

--- Roadtrip_Simulator/Assets/Scripts/SpawnController.cs
+++ Roadtrip_Simulator/Assets/Scripts/SpawnController.cs
@@ -7,35 +7,24 @@ public class SpawnController : MonoBehaviour
     public GameObject crate;
     public GameObject powerup;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         InvokeRepeating("SpawnCrateWave", 0.1f, 0.5f);
         InvokeRepeating("SpawnPowerup", 15f, 15f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void SpawnCrateWave()
     {
         Instantiate(crate, RandomSpawnTruckPosition(), Quaternion.Euler(crate.transform.rotation.x, Random.Range(-30,30), crate.transform.rotation.z));
-        // chce miec kilka pudel w jednej fali (jedno wywolanie = wiele pudel)
-        // ilosc pudel na odwolanie ma sie robic wieksza z uplywem gry
-        // na podstawie licznika punktow moze?
-        // to z czasem chyba xd
     }
 
     void SpawnPowerup()
     {
         Instantiate(powerup, RandomSpawnTruckPosition(), crate.transform.rotation);
-        // chcialbym, zeby powerup pojawial sie jako nagroda lub w momencie
-        // kiedy generator pierdolnie duzo skrzynek
     }
 
+    // ABSTRACTION
     Vector3 RandomSpawnTruckPosition()
     {
         int randomTruckX = Random.Range(-2, 3) * 5;
@@ -46,8 +35,3 @@ public class SpawnController : MonoBehaviour
     }
 }
 
-// Z axis 64, 1 of 5 trucks (X axis -10, -5, 0, 5, 10)
-// random array
-// with time i want more randomness
-// first only one crate at a time, in the end they can spawn either
-// in 4 trucks in the same time, or just by 2 at a time but very frequently
